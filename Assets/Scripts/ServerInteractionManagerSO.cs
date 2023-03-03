19fc607e6d00a6501ad79de760f54e203c3136b8
Vlad <vlad.iaremenko@gmail.com>
2023-03-03 20:31:05 +0300

    Refractor

--- Assets/Scripts/ServerInteractionManagerSO.cs
+++ Assets/Scripts/ServerInteractionManagerSO.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Text;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -58,41 +60,159 @@ namespace Assets.Scripts
         {
             while (true)
             {
-                if (!string.IsNullOrEmpty(_currentSessionID))
+                if (string.IsNullOrEmpty(_currentSessionID))
                 {
-                    var item = new Message();
-                    item.user = new User();
-                    item.user.session_id = _currentSessionID; ;
-
-                    var json = JsonConvert.SerializeObject(item);
-
-                    Debug.Log(json);
-
-                    var request = new UnityWebRequest(PingApi, "POST");
-                    byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
-                    request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
-                    request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-                    request.SetRequestHeader("Content-Type", "application/json");
-                    yield return request.SendWebRequest();
-                    Debug.Log("Status Code: " + request.responseCode);
+                    yield return new WaitForEndOfFrame();
+                    continue;
                 }
                 else
                 {
-                    _serverInteractionViewModel.HandleSessionLost();
+                    var msg = new PingRequestData(new User() { session_id = _currentSessionID});
+   
+                    var json = JsonConvert.SerializeObject(msg);
+
+                    using (UnityWebRequest webRequest = UnityWebRequest.Post(PingApi, "POST"))
+                    {
+                        webRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw(Encoding.UTF8.GetBytes(json));
+                        webRequest.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+                        webRequest.SetRequestHeader("Content-Type", "application/json");
+
+                        yield return webRequest.SendWebRequest();
+
+                        if (webRequest.responseCode == 200 && !string.IsNullOrEmpty(webRequest.downloadHandler.text))
+                        {
+                            _serverInteractionViewModel.HandleSesseionReceived();
+
+                            Debug.Log(webRequest.downloadHandler.text);
+
+                            var responce = JsonConvert.DeserializeObject<PingResponceData>(webRequest.downloadHandler.text);
+
+                            Debug.Log(responce.user.login);
+
+                            succesEvent.Invoke();
+                        }
+                        else
+                        {
+                            _serverInteractionViewModel.HandleSessionLost();
+                            _currentSessionID = string.Empty;
+                            errorEvent.Invoke();
+                        }
+                    }
                 }
 
                 yield return new WaitForSeconds(3f);
             }
         }
     }
+}
 
-    public class Message
-    {
-        public User user;
-    }
 
-    public class User
+public class PingRequestData
+{
+    public User user;
+
+    public PingRequestData(User user)
     {
-        public string session_id;
+        this.user = user;
     }
+}
+
+public class Counters
+{
+    public int friendship_requests { get; set; }
+    public int friends { get; set; }
+    public int friends_online { get; set; }
+    public int ignored { get; set; }
+    public int messages { get; set; }
+    public int rewards { get; set; }
+}
+
+public class NextQuest
+{
+    public int quest_id { get; set; }
+    public int coins { get; set; }
+}
+
+public class Rating
+{
+    public int rating_id { get; set; }
+    public int interval_id { get; set; }
+    public int rating { get; set; }
+}
+
+public class PingResponceData
+{
+    public User user { get; set; }
+    public List<int> items { get; set; }
+    public List<int> slots { get; set; }
+    public Counters counters { get; set; }
+    public List<Rating> ratings { get; set; }
+    public NextQuest next_quest { get; set; }
+}
+
+public class User
+{
+    public string session_id;
+    public int energy { get; set; }
+    public int settings_mask { get; set; }
+    public int last_activity { get; set; }
+    public bool available { get; set; }
+    public int bar_id { get; set; }
+    public string login { get; set; }
+    public bool sex { get; set; }
+    public string country_iso { get; set; }
+    public string city { get; set; }
+    public string lang { get; set; }
+    public string nickname { get; set; }
+    public int coins { get; set; }
+    public string personal_info { get; set; }
+    public string status { get; set; }
+    public int likes_count { get; set; }
+    public bool invisible { get; set; }
+    public int daily_logins { get; set; }
+    public int achiv_points { get; set; }
+    public string affiliate_id { get; set; }
+    public string ref_domain { get; set; }
+    public int orientation { get; set; }
+    public int latitude { get; set; }
+    public int longitude { get; set; }
+    public int gold { get; set; }
+    public int height { get; set; }
+    public int body_id { get; set; }
+    public int hair_id { get; set; }
+    public int eye_id { get; set; }
+    public int photo_count { get; set; }
+    public bool online { get; set; }
+    public string interests_mask { get; set; }
+    public string languiges_mask { get; set; }
+    public int profile_bonus_mask { get; set; }
+    public string fantasies { get; set; }
+    public bool ban { get; set; }
+    public bool del { get; set; }
+    public object affiliate_extra { get; set; }
+    public int avatar_storage_id { get; set; }
+    public bool login_exists { get; set; }
+    public int moder_mask { get; set; }
+    public bool bad_mail { get; set; }
+    public int login_cnt { get; set; }
+    public string utm_data { get; set; }
+    public object sponsor_tier { get; set; }
+    public object sponsor_until { get; set; }
+    public int ref_level { get; set; }
+    public bool vip { get; set; }
+    public int age { get; set; }
+    public bool birth_day { get; set; }
+    public string room_link_code { get; set; }
+    public int id { get; set; }
+    public string link_for_referral { get; set; }
+    public string referral_promo_code { get; set; }
+    public string no_avatar { get; set; }
+    public string no_avatar_thumb { get; set; }
+    public int payment_system { get; set; }
+    public int games_with_bot { get; set; }
+    public int photo_shows_left { get; set; }
+    public int invites_left { get; set; }
+    public int room_id { get; set; }
+    public string link_to_room { get; set; }
+    public int energy_remaining { get; set; }
 }
