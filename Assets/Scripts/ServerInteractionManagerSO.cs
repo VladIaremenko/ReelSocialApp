c5631e6394e5c86a40ce6dbf43e6db9e8e83a0c2
Vlad <vlad.iaremenko@gmail.com>
2023-03-03 16:56:22 +0300

    Clear session

--- Assets/Scripts/ServerInteractionManagerSO.cs
+++ Assets/Scripts/ServerInteractionManagerSO.cs
@@ -72,6 +72,7 @@ namespace Assets.Scripts
                         }
                         else
                         {
+                            _currentSessionID = string.Empty;
                             errorEvent.Invoke();
                         }
                     }
