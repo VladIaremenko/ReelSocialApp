e8cfaaf75e998ccb60cca492adfe065ae0a74092
Vlad <vlad.iaremenko@gmail.com>
2023-03-03 13:47:29 +0300

    View model

---
+++ Assets/Scripts/LoginPanelViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Coin.Assets.Misc;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [CreateAssetMenu(fileName = "LoginPanelViewModel", menuName = "SO/Type/LoginPanelViewModel", order = 1)]
+    public class LoginPanelViewModel : ScriptableObject
+    {
+        public ObservableVariable<string> CurrentLogin = new ObservableVariable<string>();
+        public ObservableVariable<string> CurrentPassword = new ObservableVariable<string>();
+    }
+}
+
+
